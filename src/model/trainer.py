513fcdf00bd14c9dcb94968004a590e13ece6b65
benjamindjian <benjamin.djian3@gmail.com>
2026-03-01 20:12:34 +0100

    Finish fit method of trainer.py, add typing

--- src/model/trainer.py
+++ src/model/trainer.py
@@ -1,9 +1,20 @@
 import torch
+from torch import nn, optim
 from torch.utils.data import DataLoader
+
+from src.model.classificator import Classificator
+
+
+class TrainHistory:
+    def __init__(self, train_loss: list[float], train_acc: list[float], val_loss: list[float], val_acc: list[float]):
+        self.train_loss = train_loss
+        self.train_acc = train_acc
+        self.val_loss = val_loss
+        self.val_acc = val_acc
 
 
 class Trainer:
-    def __init__(self, model, optimizer, criterion):
+    def __init__(self, model: Classificator, optimizer: optim.Optimizer, criterion: nn.Module):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
@@ -13,14 +24,13 @@ class Trainer:
         running_loss = 0.0
         running_acc = 0
         with torch.no_grad():
-            for inputs, labels in loader:
-
-                logits = self.model(inputs)
+            for batch in loader:
+                _, inputs, labels = batch
+                logits = self.model(inputs)[0]
                 loss = self.criterion(logits, labels)
 
                 running_loss += loss.item()
-
-                running_acc += torch.eq(logits.argmax(1), labels).sum().item()
+                running_acc += torch.eq(logits.argmax(1), labels.long()).sum().item()
 
         total_loss = running_loss / len(loader)
         total_accuracy = running_acc / len(loader)
@@ -32,24 +42,36 @@ class Trainer:
         running_loss = 0.0
         running_acc = 0
 
-        for inputs, labels in train_loader:
+        for batch in train_loader:
+            _, inputs, labels = batch
             self.optimizer.zero_grad()
 
             logits, _ = self.model(inputs)
-            loss = self.criterion(logits, labels)
+            loss = self.criterion(logits, labels.long())
 
             loss.backward()
             self.optimizer.step()
 
             running_loss += loss.item()
-            running_acc += torch.eq(logits.argmax(1), labels).sum().item()
+            running_acc += torch.eq(logits.argmax(1), labels.long()).sum().item()
 
         train_loss = running_loss / len(train_loader)
         train_accuracy = running_acc / len(train_loader)
 
         return train_loss, train_accuracy
 
-    def fit(self, train_loader: DataLoader, val_loader: DataLoader, epochs=10):
+    def fit(self, train_loader: DataLoader, val_loader: DataLoader, epochs: int) -> TrainHistory:
+        train_loss_list = []
+        train_acc_list = []
+        val_loss_list = []
+        val_acc_list = []
         for epoch in range(epochs):
-            train_loss = self.train_epoch(train_loader)
+            train_loss, train_acc = self.train_epoch(train_loader)
             val_loss, val_acc = self.evaluate(val_loader)
+
+            train_loss_list.append(train_loss)
+            train_acc_list.append(train_acc)
+            val_loss_list.append(val_loss)
+            val_acc_list.append(val_acc)
+
+        return TrainHistory(train_loss_list, train_acc_list, val_loss_list, val_acc_list)
