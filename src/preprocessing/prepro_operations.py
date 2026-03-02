2caf10ce52370cec3235ece34b90516ae575202e
benjamindjian <benjamin.djian3@gmail.com>
2026-03-02 19:45:59 +0100

    Filter out target and sens attr from preprocessing operations

--- src/preprocessing/prepro_operations.py
+++ src/preprocessing/prepro_operations.py
@@ -1,3 +1,4 @@
+import warnings
 from abc import ABC, abstractmethod
 
 import pandas as pd
@@ -14,7 +15,7 @@ class PreprocessingOperation(ABC):
         pass
 
     @abstractmethod
-    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
         pass
 
 
@@ -24,9 +25,10 @@ class MakeCategorical(PreprocessingOperation):
         self.lb = lb
         self.ub = ub
 
-    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
         # We select all columns that are not number or bool dtype
         categorical_columns = df.select_dtypes(exclude=['number', 'bool']).columns.tolist()
+        categorical_columns = [c for c in categorical_columns if c not in [target_name, sens_attr_name]]
 
         # Categorical columns are also numerical columns with 3 to numeric_as_categorical_max_thr different values
         for c in df.select_dtypes(include=['number']).columns:
@@ -40,9 +42,10 @@ class MakeCategorical(PreprocessingOperation):
 class Scale(PreprocessingOperation):
     """Scale all numerical column between 0 and 1"""
 
-    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
         scaler = MinMaxScaler()
         numeric_columns = [c for c in df.columns if (is_numeric_dtype(df[c]))]
+        numeric_columns = [c for c in numeric_columns if c not in [target_name, sens_attr_name]]
         if numeric_columns:
             new_df = df.copy()
             new_df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
@@ -55,45 +58,58 @@ class Scale(PreprocessingOperation):
 class ToFloat(PreprocessingOperation):
     """Convert all values of dataset to float type"""
 
-    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
         return df.astype(float)
 
-# class CorrelationRemoverPrepro(PreprocessingOperation):
-#     def run(self, df: pd.DataFrame) -> pd.DataFrame:
-#         from fairlearn.preprocessing import CorrelationRemover
-#
-#         target_col = df[self.target]
-#         index_col = df.index
-#
-#         cr = CorrelationRemover(sensitive_feature_ids=[protec_attr])
-#         X_cr = cr.fit_transform(X=df, y=target_col)
-#         cr_col = list(df.columns)
-#         cr_col.remove(protec_attr)
-#         X_cr = pd.DataFrame(X_cr, columns=cr_col)
-#         X_cr.index = index_col
-#         X_cr[protec_attr] = df[protec_attr]
-#         X_cr[self.target] = target_col
-#
-#         # column order
-#         X_cr = X_cr[list(df.columns)]
-#         df = X_cr
-#
-#
-# class DisparateImpactRemoverPrepro(PreprocessingOperation):
-#     def run(self, df: pd.DataFrame) -> pd.DataFrame:
-#         from aif360.datasets import StandardDataset
-#         from aif360.algorithms.preprocessing import DisparateImpactRemover
-#
-#         standard_data = StandardDataset(df=df,
-#                                         label_name=self.target,
-#                                         protected_attribute_names=[protec_attr],
-#                                         favorable_classes=favorable_classes,
-#                                         # Label that is considered as positive
-#                                         privileged_classes=privileged_classes)  # protected attr that are considered privileged
-#
-#         dir_ = DisparateImpactRemover(sensitive_attribute=protec_attr)
-#         data_dir = dir_.fit_transform(standard_data)
-#         data_dir, _ = data_dir.convert_to_dataframe()
-#         data_dir = data_dir[list(df.columns)]
-#         df = data_dir
-#         df.index = df.index.astype(int)
+
+class TargetAtTheEnd(PreprocessingOperation):
+    """Put target column at the end of the feature"""
+
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
+        target = df[target_name]
+        new_df = df.copy()
+        new_df.drop(columns=[target_name], inplace=True)
+        with warnings.catch_warnings(action="ignore"):
+            new_df.insert(len(new_df.columns), target_name, target)
+        return new_df
+
+
+class CorrelationRemoverPrepro(PreprocessingOperation):
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
+        from fairlearn.preprocessing import CorrelationRemover
+
+        target_col = df[target_name]
+        index_col = df.index
+
+        cr = CorrelationRemover(sensitive_feature_ids=[sens_attr_name])
+        X_cr = cr.fit_transform(X=df, y=target_col)
+        cr_col = list(df.columns)
+        cr_col.remove(sens_attr_name)
+        X_cr = pd.DataFrame(X_cr, columns=cr_col)
+        X_cr.index = index_col
+        X_cr[sens_attr_name] = df[sens_attr_name]
+        X_cr[target_name] = target_col
+
+        # column order
+        X_cr = X_cr[list(df.columns)]
+        return X_cr
+
+
+class DisparateImpactRemoverPrepro(PreprocessingOperation):
+    def run(self, df: pd.DataFrame, target_name: str, sens_attr_name: str) -> pd.DataFrame:
+        from aif360.datasets import StandardDataset
+        from aif360.algorithms.preprocessing import DisparateImpactRemover
+
+        standard_data = StandardDataset(df=df,
+                                        label_name=target_name,
+                                        protected_attribute_names=[sens_attr_name],
+                                        favorable_classes=favorable_classes,
+                                        # Label that is considered as positive
+                                        privileged_classes=privileged_classes)  # protected attr that are considered privileged
+
+        dir_ = DisparateImpactRemover(sensitive_attribute=sens_attr_name)
+        data_dir = dir_.fit_transform(standard_data)
+        data_dir, _ = data_dir.convert_to_dataframe()
+        data_dir = data_dir[list(df.columns)]
+        data_dir.index = data_dir.index.astype(int)
+        return data_dir
--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,11 +1,12 @@
 from abc import ABC
 
 import pandas as pd
+import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
 from src.preprocessing.dataset import IndexDataset
-from src.preprocessing.prepro_operations import PreprocessingOperation, MakeCategorical, Scale, ToFloat
+from src.preprocessing.prepro_operations import PreprocessingOperation, MakeCategorical, Scale, ToFloat, TargetAtTheEnd
 from src.utils.env import ADULT_DATA_PATH, LAW_DATA_PATH, GERMAN_DATA_PATH, ADULT_TARGET, GERMAN_TARGET, LAW_TARGET
 from src.utils.logger import LoggerFactory
 
@@ -27,7 +28,9 @@ class Preprocessing(ABC):
         if not self.operations:
             raise ValueError("Preprocessing operations are not defined yet")
         for op in self.operations:
-            self.df = op.run(self.df)
+            self.df = op.run(self.df,
+                             target_name=self.target_column,
+                             sens_attr_name=self.sens_attr_column)
 
     def generate_dataset(self, new_df: pd.DataFrame) -> IndexDataset:
         dataset = IndexDataset(new_df, target_column=self.target_column, sens_attr_column=self.sens_attr_column)
@@ -45,29 +48,30 @@ class Preprocessing(ABC):
         val_dataset = self.generate_dataset(val_df)
         test_dataset = self.generate_dataset(test_df)
 
-        train_loader = DataLoader(train_dataset, batch_size=batch_size)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size)
+        torch.manual_seed(seed)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
         return train_loader, val_loader, test_loader
 
 
 class AdultPreprocessing(Preprocessing):
     def __init__(self, sens_attr_column: str):
-        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat()]
+        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat(), TargetAtTheEnd()]
         df = pd.read_csv(ADULT_DATA_PATH, index_col='inputId')
         super().__init__(df, operations, ADULT_TARGET, sens_attr_column)
 
 
 class GermanCreditPreprocessing(Preprocessing):
     def __init__(self, sens_attr_column: str):
-        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat()]
+        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat(), TargetAtTheEnd()]
         df = pd.read_csv(GERMAN_DATA_PATH, index_col='inputId')
         super().__init__(df, operations, GERMAN_TARGET, sens_attr_column)
 
 
 class LawSchoolPreprocessing(Preprocessing):
     def __init__(self, sens_attr_column: str):
-        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat()]
+        operations = [MakeCategorical(lb=3, ub=5), Scale(), ToFloat(), TargetAtTheEnd()]
         df = pd.read_csv(LAW_DATA_PATH, index_col='inputId')
         super().__init__(df, operations, LAW_TARGET, sens_attr_column)
