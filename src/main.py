5ee93a5833c493960e8e3b3e99d5c3447fffa084
benjamindjian <benjamin.djian3@gmail.com>
2026-03-02 20:24:58 +0100

    Update config.yaml and use main function in main.py

--- src/main.py
+++ src/main.py
@@ -2,26 +2,22 @@ import argparse
 from pathlib import Path
 
 from src.experiment.experiment import Experiment
-from src.visualisation.visualisation import PlotCPDFractionPop
+from src.utils.logger import LoggerFactory
+
+
+def main(config_file: Path):
+    experiment_dir = config_file.parent
+    log_file = experiment_dir / "info.log"
+    LoggerFactory(log_file=log_file)
+    logger = LoggerFactory.get_logger(__name__)
+    logger.info(f"Starting experiment in {experiment_dir}")
+    exp = Experiment(config_path)
+    exp.run(config_path.parent)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", required=True, type=str, help="Path to exp config")
     args = parser.parse_args()
     config_path = Path(args.config)
-    exp = Experiment(config_path)
-
-    a, b, c, d = exp.run()
-    graph_h0 = PlotCPDFractionPop(serie_1=a, serie_2=c, size=(7, 7),
-                                  title="Group 0 & 1 from H0",
-                                  label_1="Group 0",
-                                  label_2="Group 1")
-
-    graph_h1 = PlotCPDFractionPop(serie_1=b, serie_2=d, size=(7, 7),
-                                  title="Group 0 & 1 from H1",
-                                  label_1="Group 0",
-                                  label_2="Group 1")
-
-    graph_h0.plot()
-
-    graph_h1.plot()
+    main(config_path)
