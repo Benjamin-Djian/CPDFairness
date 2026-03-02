262f24f63fe7b6655391945c76ad504baf175529
benjamindjian <benjamin.djian3@gmail.com>
2026-03-02 19:45:27 +0100

    Use log file for logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,16 +1,26 @@
 import logging
 import sys
+from pathlib import Path
 
 import src.utils.env as e
 
 
 class LoggerFactory:
-    def __init__(self, log_format=e.LOG_FORMAT):
-        root_logger = logging.getLogger()
-        hand = logging.StreamHandler(sys.stdout)
-        hand.setFormatter(logging.Formatter(log_format))
+    _initialized = False
 
-        root_logger.addHandler(hand)
+    def __init__(self, log_file: Path | None = None, log_format=e.LOG_FORMAT):
+        if LoggerFactory._initialized:
+            return
+        root_logger = logging.getLogger()
+        root_logger.handlers.clear()
+        if log_file:
+            handler = logging.FileHandler(log_file)
+        else:
+            handler = logging.StreamHandler(sys.stdout)
+
+        handler.setFormatter(logging.Formatter(log_format))
+
+        root_logger.addHandler(handler)
 
         logging_levels = {
             'critical': logging.CRITICAL,
@@ -23,6 +33,8 @@ class LoggerFactory:
         }
         root_logger.setLevel(logging_levels[e.LOG_LEVEL])
 
+        LoggerFactory._initialized = True
+
     @staticmethod
-    def get_logger(name: str):
+    def get_logger(name: str) -> logging.Logger:
         return logging.getLogger(name)
