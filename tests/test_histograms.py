57dd46a2f68ae727de582b23fbbd084b67de9787
benjamindjian <benjamin.djian3@gmail.com>
2026-03-03 18:57:27 +0100

    More test for hist_prob calculations

--- tests/test_histograms.py
+++ tests/test_histograms.py
@@ -40,19 +40,19 @@ class TestUniBinHistogram:
         with pytest.raises(ValueError, match="only one bin"):
             UniBinHistogram(node_id=1, bins=bins, freq=freq)
 
-    def test_compute_hist_prob_at_lower_bound(self):
-        """Test compute_hist_prob returns 1.0 at lower bound."""
-        bins = np.array([0.0, 0.0])
+    def test_compute_hist_prob_at_exact_lower_bound(self):
+        """Test compute_hist_prob returns 1.0 at exact lower bound."""
+        bins = np.array([0.5, 0.5])
         freq = np.array([10.0])
 
         hist = UniBinHistogram(node_id=1, bins=bins, freq=freq)
 
-        prob = hist.compute_hist_prob(0.0)
+        prob = hist.compute_hist_prob(0.5)
 
         assert prob == 1.0
 
-    def test_compute_hist_prob_away_from_lower_bound(self):
-        """Test compute_hist_prob returns low prob away from lower bound."""
+    def test_compute_hist_prob_above_lower_bound(self):
+        """Test compute_hist_prob returns LOW_SMOOTHED_PROB when above lower bound."""
         bins = np.array([0.0, 0.0])
         freq = np.array([10.0])
 
@@ -60,7 +60,42 @@ class TestUniBinHistogram:
 
         prob = hist.compute_hist_prob(0.5)
 
-        assert prob < 1.0
+        assert prob == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_below_lower_bound(self):
+        """Test compute_hist_prob returns LOW_SMOOTHED_PROB when below lower bound."""
+        bins = np.array([0.5, 0.5])
+        freq = np.array([10.0])
+
+        hist = UniBinHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob = hist.compute_hist_prob(0.0)
+
+        assert prob == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_negative_lower_bound(self):
+        """Test compute_hist_prob with negative lower bound."""
+        bins = np.array([-1.0, -1.0])
+        freq = np.array([5.0])
+
+        hist = UniBinHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob_at = hist.compute_hist_prob(-1.0)
+        prob_above = hist.compute_hist_prob(0.0)
+
+        assert prob_at == 1.0
+        assert prob_above == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_slightly_above_lower_bound(self):
+        """Test compute_hist_prob slightly above lower_bound returns LOW_SMOOTHED_PROB."""
+        bins = np.array([0.0, 0.0])
+        freq = np.array([10.0])
+
+        hist = UniBinHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob = hist.compute_hist_prob(e.EPSILON * 2)
+
+        assert prob == e.LOW_SMOOTHED_PROB
 
 
 class TestMultiBinsHistogram:
@@ -77,6 +112,21 @@ class TestMultiBinsHistogram:
         assert np.array_equal(hist.bins, bins)
         assert np.array_equal(hist.freq, freq)
 
+    def test_compute_hist_prob_on_bin_edges(self):
+        """Test compute_hist_prob on exact bin edges."""
+        bins = np.array([0.0, 0.5, 1.0])
+        freq = np.array([5.0, 20.0])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob0 = hist.compute_hist_prob(0.0)
+        prob1 = hist.compute_hist_prob(0.5)
+        prob2 = hist.compute_hist_prob(1.0)
+
+        assert prob0 == 0.2
+        assert prob1 == 0.8
+        assert prob2 == e.LOW_SMOOTHED_PROB
+
     def test_compute_hist_prob(self):
         """Test compute_hist_prob returns probability."""
         bins = np.array([0.0, 0.5, 1.0])
@@ -88,16 +138,77 @@ class TestMultiBinsHistogram:
 
         assert prob == 0.2
 
-    def test_compute_hist_prob_at_lower_bound(self):
-        """Test compute_hist_prob at lower bound."""
+    def test_compute_hist_prob_second(self):
+        """Test compute_hist_prob returns probability."""
         bins = np.array([0.0, 0.5, 1.0])
         freq = np.array([5.0, 20.0])
 
         hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
 
+        prob = hist.compute_hist_prob(0.75)
+
+        assert prob == 0.8
+
+    def test_compute_hist_prob_below_lower_bound(self):
+        """Test compute_hist_prob below lower bound returns LOW_SMOOTHED_PROB."""
+        bins = np.array([0.5, 1.0, 1.5])
+        freq = np.array([10.0, 10.0])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
         prob = hist.compute_hist_prob(0.0)
 
-        assert prob == 0.2
+        assert prob == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_above_upper_bound(self):
+        """Test compute_hist_prob above upper bound returns LOW_SMOOTHED_PROB."""
+        bins = np.array([0.0, 0.5, 1.0])
+        freq = np.array([5.0, 10.0])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob = hist.compute_hist_prob(2.0)
+
+        assert prob == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_zero_frequency_bin(self):
+        """Test compute_hist_prob with zero frequency bin returns LOW_SMOOTHED_PROB."""
+        bins = np.array([0.0, 0.33, 0.66, 1.0])
+        freq = np.array([10.0, 0.0, 10.0])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob = hist.compute_hist_prob(0.5)
+
+        assert prob == e.LOW_SMOOTHED_PROB
+
+    def test_compute_hist_prob_negative_lower_bound(self):
+        """Test compute_hist_prob with negative lower bound."""
+        bins = np.array([-2.0, -1.0, 0.0])
+        freq = np.array([5, 15])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob1 = hist.compute_hist_prob(-2.0)
+        prob2 = hist.compute_hist_prob(-1.0)
+        prob3 = hist.compute_hist_prob(-1.5)
+
+        assert prob1 == 0.25
+        assert prob2 == 0.75
+        assert prob3 == 0.25
+
+    def test_compute_hist_prob_slightly_above_lower_bound(self):
+        """Test compute_hist_prob slightly above lower_bound returns LOW_SMOOTHED_PROB."""
+        bins = np.array([0.0, 1.0])
+        freq = np.array([10.0])
+
+        hist = MultiBinsHistogram(node_id=1, bins=bins, freq=freq)
+
+        prob_below = hist.compute_hist_prob(-2*e.EPSILON)
+        prob_above = hist.compute_hist_prob(1 + e.EPSILON)
+
+        assert prob_below == e.LOW_SMOOTHED_PROB
+        assert prob_above == e.LOW_SMOOTHED_PROB
 
 
 class TestHistogramConstructor:
